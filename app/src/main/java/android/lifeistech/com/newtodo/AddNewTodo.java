790bbc64930b9b59bf0718bb7fe7e05eef5982d0
masami-butchy <26708399+masami-butchy@users.noreply.github.com>
2018-09-21 15:47:29 +0900

    First Commit

---
+++ app/src/main/java/android/lifeistech/com/newtodo/AddNewTodo.java
@@ -0,0 +1,14 @@
+package android.lifeistech.com.newtodo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNewTodo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_todo);
+    }
+
+}
